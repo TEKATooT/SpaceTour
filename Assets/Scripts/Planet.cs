f0988e77b6d298d38db4dfdac221072d70e3dd2c
TEKATooT <63135446+TEKATooT@users.noreply.github.com>
2024-10-25 21:58:39 +0300

    PlayerMoverAdded

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -1,14 +1,24 @@
+using System;
 using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
+    public event Action Destroyed;
+
     private void Start()
     {
-        
+
     }
 
     private void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroyed?.Invoke();
+
+        Debug.Log("TRIGER");
     }
 }
--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -5,13 +5,59 @@ using UnityEngine.InputSystem;
 
 public class PlayerMover : MonoBehaviour
 {
+    [SerializeField] private float _forwardSpeed = 3f;
+    [SerializeField] private Planet _targetPlanet;
+
+    private Transform _transform;
+    private Vector3 _deadLine;
+
+    private readonly float _startHeight = 5f;
+
+    private void Awake()
+    {
+        _transform = transform;
+    }
+
+    private void OnEnable()
+    {
+        _targetPlanet.Destroyed += MoveBoost;
+    }
+
+    private void OnDisable()
+    {
+        _targetPlanet.Destroyed -= MoveBoost;
+    }
+
     private void Start()
     {
-        
+
     }
 
     private void Update()
     {
-        
+        _deadLine = new Vector3(_transform.position.x, _targetPlanet.transform.position.y, _targetPlanet.transform.position.z);
+
+        _transform.position = Vector3.MoveTowards(_transform.position, _deadLine, _forwardSpeed * Time.deltaTime);
+
+        //_transform.LookAt(_deadLine);
+    }
+
+    private void MoveBoost()
+    {
+        Vector3 defaultHeight = _transform.position;
+
+        defaultHeight.y = _startHeight;
+
+        _transform.position = Vector3.Lerp(_transform.position, defaultHeight, 1);
+    }
+
+    public void OnLeftStrafe()
+    {
+        _transform.Translate(Vector3.left * 0.2f);
+    }
+
+    public void OnRightStrafe()
+    {
+        _transform.Translate(Vector3.right * 0.2f);
     }
 }
