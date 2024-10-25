eeb591b101c7bdc10fe92eb601aa9a1c772fcadf
TekinTooT <dendendendenisss@gmail.com>
2024-10-25 03:15:42 +0300

    AddTestScene

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+
+public class PlayerMover : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
